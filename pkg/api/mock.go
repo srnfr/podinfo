d8b6c4c3194fe48c3e713812b0cca93d2af33cc4
srnfr <stephane@reytan.fr>
2021-02-07 09:24:55 +0100

    Salutations changées

--- pkg/api/mock.go
+++ pkg/api/mock.go
@@ -18,7 +18,7 @@ func NewMockServer() *Server {
 		HttpClientTimeout:         30 * time.Second,
 		UIColor:                   "blue",
 		UIPath:                    ".ui",
-		UIMessage:                 "Salutations",
+		UIMessage:                 "Salutations (depuis Paris)",
 		Hostname:                  "localhost",
 	}
 
